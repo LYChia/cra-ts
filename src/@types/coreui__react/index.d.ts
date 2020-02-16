b8eca570d895bb46345f385bcd5d69cb71c2003d
LYChia <46305376+LYChia@users.noreply.github.com>
2020-02-16 18:57:02 +0800

    @types
    @types

---
+++ src/@types/coreui__react/index.d.ts
@@ -0,0 +1,35 @@
+declare module '@coreui/react' {
+    declare class AppHeaderDropdown<T = { [key: string]: any }> extends React.Component<any>{ }
+    declare class AppAside<T = { [key: string]: any }> extends React.Component<any>{ }
+    declare class AppFooter<T = { [key: string]: any }> extends React.Component<any>{ }
+    declare class AppHeader<T = { [key: string]: any }> extends React.Component<any>{ }
+    declare class AppSidebar<T = { [key: string]: any }> extends React.Component<any>{ }
+    declare class AppSidebarFooter<T = { [key: string]: any }> extends React.Component<any>{ }
+    declare class AppSidebarForm<T = { [key: string]: any }> extends React.Component<any>{ }
+    declare class AppSidebarHeader<T = { [key: string]: any }> extends React.Component<any>{ }
+    declare class AppSidebarMinimizer<T = { [key: string]: any }> extends React.Component<any>{ }
+    declare class AppBreadcrumb2<T = { [key: string]: any }> extends React.Component<any>{ }
+    declare class AppSidebarNav2<T = { [key: string]: any }> extends React.Component<any>{ }
+    declare class AppAsideToggler<T = { [key: string]: any }> extends React.Component<any>{ }
+    declare class AppNavbarBrand<T = { [key: string]: any }> extends React.Component<any>{ }
+    declare class AppSidebarToggler<T = { [key: string]: any }> extends React.Component<any>{ }
+    declare class AppSwitch<T = { [key: string]: any }> extends React.Component<any>{ }
+
+    export {
+        AppHeaderDropdown,
+        AppAside,
+        AppFooter,
+        AppHeader,
+        AppSidebar,
+        AppSidebarFooter,
+        AppSidebarForm,
+        AppSidebarHeader,
+        AppSidebarMinimizer,
+        AppBreadcrumb2,
+        AppSidebarNav2,
+        AppAsideToggler,
+        AppNavbarBrand,
+        AppSidebarToggler,
+        AppSwitch
+    }
+} 
